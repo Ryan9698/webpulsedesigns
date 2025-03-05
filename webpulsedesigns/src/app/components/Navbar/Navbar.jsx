aa28bd2089cb45f137dba6b52139adabd05168c6
Ryan Caltabiano <rcaltabiano90@gmail.com>
2025-03-05 16:55:32 -0500

    Added some JSX

--- webpulsedesigns/src/app/components/Navbar/Navbar.jsx
+++ webpulsedesigns/src/app/components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
 import Link from 'next/link';
 
 export default function Navbar() {
+  const navLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/services', label: 'Services' },
+    { href: '/portfolio', label: 'Portfolio' },
+    { href: 'about', label: 'Contact' },
+  ];
+  const linkStyles = 'hover:text-red-900 text-white';
   return (
-    <div className="flex flex-row gap-4 md:justify-center text-white md:text-2xl p-4">
-      <Link href="/">Home</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/services">Services</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+    <div>
+      <ul className="flex md:justify-center gap-4">
+        {navLinks.map(({ href, label }) => (
+          <li key={href}>
+            <Link href={href} className={linkStyles}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
