6c5ddebfb5c4b2032fe0bffb15ef4114d3c34aae
Ryan Caltabiano <rcaltabiano90@gmail.com>
2025-05-09 19:20:47 -0400

    Changed motion footer to fade in

--- webpulsedesigns/src/components/general/SlideIn/SlideInFooter.jsx
+++ webpulsedesigns/src/components/general/SlideIn/SlideInFooter.jsx
@@ -4,19 +4,11 @@ import { motion } from 'framer-motion';
 export default function SlideInFooter({ children, className = '', ...props }) {
   return (
     <motion.div
-      initial={{ y: 100, opacity: 0 }}
-      animate={{
-        y: 0,
-        opacity: 1,
-        transition: { duration: 1.25, ease: 'easeOut' },
-      }}
-      exit={{
-        y: 100,
-        opacity: 0,
-        transition: { duration: 0.5, ease: 'easeIn' },
-      }}
-      className={`fixed bottom-0 left-0 right-0 z-50 will-change-transform ${className}`}
-      {...props}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.6 }}
+      className="fixed bottom-0 left-0 right-0 z-[9999] bg-black/80 backdrop-blur w-full"
     >
       {children}
     </motion.div>
